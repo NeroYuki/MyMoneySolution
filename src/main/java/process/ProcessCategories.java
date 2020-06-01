341c205618fedc8809c54581ad0c97267663eace
ahahavip <55023509+ahahavip@users.noreply.github.com>
2020-06-01 15:57:56 +0700

    1/3 3.57
    :vvv

--- src/main/java/process/ProcessCategories.java
+++ src/main/java/process/ProcessCategories.java
@@ -4,6 +4,8 @@ import database.*;
 import exception.DatabaseException;
 import model.Category;
 
+import java.util.ArrayList;
+
 public class ProcessCategories {
     public static boolean saveCategories(String Name,String FileName,String Info,String Type){
 
@@ -43,6 +45,38 @@ public class ProcessCategories {
         catch (DatabaseException De){
             System.out.println("chuong trinh loi khi dua data vao database");
             return false;
+        }
+    }
+    public static String[] getIncomeCategories() throws DatabaseException{
+        try {
+            ArrayList<Category> categories = DatabaseCategories.getIncomeCategory();
+            String[] categoriesname=new String[categories.size()];
+            for(Category cat: categories) {
+                categoriesname[categories.indexOf(cat)]=cat.getName();
+            }
+            return  categoriesname;
+        }
+        catch (DatabaseException de){
+            throw de;
+        }
+        catch (Exception e){
+            throw e;
+        }
+    }
+    public static String[] getExpenseCategories() throws DatabaseException{
+        try {
+            ArrayList<Category> categories = DatabaseCategories.getExpenseCategory();
+            String[] categoriesname=new String[categories.size()];
+            for(Category cat: categories) {
+                categoriesname[categories.indexOf(cat)]=cat.getName();
+            }
+            return  categoriesname;
+        }
+        catch (DatabaseException de){
+            throw de;
+        }
+        catch (Exception e){
+            throw e;
         }
     }
 }
