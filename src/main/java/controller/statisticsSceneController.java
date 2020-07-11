0bf1b88382fba7fdc6733bb28b6af5b6aca136fc
lhmson <lehoangminhson123@gmail.com>
2020-07-12 00:16:28 +0700

    Edit statistic chart of income expense to line

--- src/main/java/controller/statisticsSceneController.java
+++ src/main/java/controller/statisticsSceneController.java
@@ -1,14 +1,9 @@
 package controller;
 
-import exception.DatabaseException;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.ChoiceDialog;
@@ -18,14 +13,13 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import model.Expense;
-import model.Income;
-import model.Transaction;
-import process.ProcessCategories;
 import scenes.*;
 
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
 
 public class statisticsSceneController implements Initializable {
     @FXML
@@ -95,119 +89,143 @@ public class statisticsSceneController implements Initializable {
         Tooltip.install(planBtn, new Tooltip("Add financial goal"));
 
         // display line chart of balance all time
-        displayLineChart();
+        displayBalanceChart();
 
-        // display bar chart in tab income and expense
-        displayBarChart();
+        // display line chart in tab income and expense
+        displayIncomeChart();
+        displayExpenseChart();
 
     }
 
 
 
-    @FXML
-    public BarChart<String,Double> incomeCategoriesBarChart;
-    public BarChart<String,Double> expenseCategoriesBarChart;
-    public CategoryAxis incomeCategoriesAxis;
-    public CategoryAxis expenseCategoriesAxis;
-
-    private ObservableList<String> incomeCategoryNames = FXCollections.observableArrayList();
-    private ObservableList<String> expenseCategoryNames = FXCollections.observableArrayList();
-
-    // init a list of transaction to get from database input
-    private ObservableList<Transaction> transactionList = FXCollections.observableArrayList(); // list of transaction
-
-    public void displayBarChart() {
-        // Get an array with the categories income
-//        String[] incomeCategories = {"Salary","Bonus","Award"};
-        try {
-            String[] incomeCategories = ProcessCategories.getIncomeCategoriesName();
-            //TODO: get right list from own database
-//        String[] expenseCategories = {"Shopping","Food and Beverage","Movie and show"};
-            String[] expenseCategories = ProcessCategories.getExpenseCategoriesName();
-            //TODO: get right list from own database
-
-            // Convert it to a list and add it to our ObservableList of categories
-            incomeCategoryNames.addAll(Arrays.asList(incomeCategories));
-            expenseCategoryNames.addAll(Arrays.asList(expenseCategories));
-
-            // Assign the categories names as categories for the horizontal axis.
-            incomeCategoriesAxis.setCategories(incomeCategoryNames);
-            expenseCategoriesAxis.setCategories(expenseCategoryNames);
-
-            //TODO: get right list from own database
-//        transactionList.add(
-//                new Income(LocalDate.of(2004,1,5),50000, "School giving scholarship", "Bonus"));
-//        transactionList.add(new Expense(LocalDate.of(2004,4,15),-20000, "Buy a phone in FPT", "Shopping"));
-//        transactionList.add(new Income(LocalDate.of(2004,5,6),120000, "Salary May", "Salary"));
-//        transactionList.add(new Expense(LocalDate.of(2004,11,23),-35000, "Restaurant district 5", "Food and Beverage"));
+////    public BarChart<String,Double> incomeCategoriesBarChart;
+////    public BarChart<String,Double> expenseCategoriesBarChart;
+//    public CategoryAxis incomeCategoriesAxis;
+//    public CategoryAxis expenseCategoriesAxis;
 //
-//        transactionList.add(
-//                new Income(LocalDate.of(2004,1,5),50000, "School giving scholarship", "Bonus"));
-//        transactionList.add(new Expense(LocalDate.of(2004,4,15),-20000, "Buy a phone in FPT", "Shopping"));
-//        transactionList.add(new Income(LocalDate.of(2004,5,6),120000, "Salary May", "Salary"));
-//        transactionList.add(new Expense(LocalDate.of(2004,11,23),-35000, "Restaurant district 5", "Food and Beverage"));
-            //           transactionList.addAll(ProcessTransactionScene.getTransactionsInfo());
-            // Count the total value of a categories in an account
-            Double[] incomeValueTotal = new Double[incomeCategories.length];
-            Double[] expenseValueTotal = new Double[expenseCategories.length];
-            //TODO: apply the number of categories from database
-
-            // set initial value of 0 to calculate total then
-            for (int i = 0; i < incomeValueTotal.length; i++) {
-                incomeValueTotal[i] = (double) 0;
-            }
-            for (int i = 0; i < expenseValueTotal.length; i++) {
-                expenseValueTotal[i] = (double) 0;
-            }
-
-            // calculate there
-            for (Transaction t : transactionList) {
-                String incomeSpecifiedCategory = "";
-                String expenseSpecifiedCategory = "";
-                Double value = Math.abs(t.getTransValue());
-                // check if it is equal to Income
-                if (t.getClass().getName().equals("model.Income")) {
-                    incomeSpecifiedCategory = ((Income) t).getCategoryName();
-                } else if (t.getClass().getName().equals("model.Expense")) {
-                    expenseSpecifiedCategory = ((Expense) t).getCategoryName();
-                }
-                System.out.println(t.getClass().getName());
-                for (int i = 0; i < incomeCategories.length; i++) {
-                    if (incomeSpecifiedCategory.equals(incomeCategories[i])) {
-                        incomeValueTotal[i] += value;
-                        System.out.println("income categories " + incomeCategories[i] + incomeValueTotal[i]);
-                    } else if (expenseSpecifiedCategory.equals(expenseCategories[i])) {
-                        expenseValueTotal[i] += value;
-                        System.out.println("expense categories " + expenseCategories[i] + expenseValueTotal[i]);
-                    }
-                }
-            }
-
-            XYChart.Series<String, Double> incomeCategorySeries = new XYChart.Series<>();
-            XYChart.Series<String, Double> expenseCategorySeries = new XYChart.Series<>();
-
-            // Create a XYChart.Data object for every category, then add it to the series.
-            for (int i = 0; i < incomeValueTotal.length; i++) {
-                incomeCategorySeries.getData().add(new XYChart.Data<>(incomeCategoryNames.get(i), incomeValueTotal[i]));
-            }
-            for (int i = 0; i < expenseValueTotal.length; i++) {
-                expenseCategorySeries.getData().add(new XYChart.Data<>(expenseCategoryNames.get(i), expenseValueTotal[i]));
-            }
-
-            incomeCategoriesBarChart.getData().add(incomeCategorySeries);
-            expenseCategoriesBarChart.getData().add(expenseCategorySeries);
-        }
-        catch (DatabaseException de) {
-            System.out.println(de.getErrorCodeMessage());
-        }
-//        catch (ProcessExeption pe){
-//            System.out.println(pe.getErrorCodeMessage());
+//    private ObservableList<String> incomeCategoryNames = FXCollections.observableArrayList();
+//    private ObservableList<String> expenseCategoryNames = FXCollections.observableArrayList();
+//
+//    // init a list of transaction to get from database input
+//    private ObservableList<Transaction> transactionList = FXCollections.observableArrayList(); // list of transaction
+//
+//    public void displayBarChart() {
+//        // Get an array with the categories income
+////        String[] incomeCategories = {"Salary","Bonus","Award"};
+//        try {
+//            String[] incomeCategories = ProcessCategories.getIncomeCategoriesName();
+//            //TODO: get right list from own database
+////        String[] expenseCategories = {"Shopping","Food and Beverage","Movie and show"};
+//            String[] expenseCategories = ProcessCategories.getExpenseCategoriesName();
+//            //TODO: get right list from own database
+//
+//            // Convert it to a list and add it to our ObservableList of categories
+//            incomeCategoryNames.addAll(Arrays.asList(incomeCategories));
+//            expenseCategoryNames.addAll(Arrays.asList(expenseCategories));
+//
+//            // Assign the categories names as categories for the horizontal axis.
+//            incomeCategoriesAxis.setCategories(incomeCategoryNames);
+//            expenseCategoriesAxis.setCategories(expenseCategoryNames);
+//
+//            // Count the total value of a categories in an account
+//            Double[] incomeValueTotal = new Double[incomeCategories.length];
+//            Double[] expenseValueTotal = new Double[expenseCategories.length];
+//            //TODO: apply the number of categories from database
+//
+//            // set initial value of 0 to calculate total then
+//            for (int i = 0; i < incomeValueTotal.length; i++) {
+//                incomeValueTotal[i] = (double) 0;
+//            }
+//            for (int i = 0; i < expenseValueTotal.length; i++) {
+//                expenseValueTotal[i] = (double) 0;
+//            }
+//
+//            // calculate there
+//            for (Transaction t : transactionList) {
+//                String incomeSpecifiedCategory = "";
+//                String expenseSpecifiedCategory = "";
+//                Double value = Math.abs(t.getTransValue());
+//                // check if it is equal to Income
+//                if (t.getClass().getName().equals("model.Income")) {
+//                    incomeSpecifiedCategory = ((Income) t).getCategoryName();
+//                } else if (t.getClass().getName().equals("model.Expense")) {
+//                    expenseSpecifiedCategory = ((Expense) t).getCategoryName();
+//                }
+//                System.out.println(t.getClass().getName());
+//                for (int i = 0; i < incomeCategories.length; i++) {
+//                    if (incomeSpecifiedCategory.equals(incomeCategories[i])) {
+//                        incomeValueTotal[i] += value;
+//                        System.out.println("income categories " + incomeCategories[i] + incomeValueTotal[i]);
+//                    } else if (expenseSpecifiedCategory.equals(expenseCategories[i])) {
+//                        expenseValueTotal[i] += value;
+//                        System.out.println("expense categories " + expenseCategories[i] + expenseValueTotal[i]);
+//                    }
+//                }
+//            }
+//
+//            XYChart.Series<String, Double> incomeCategorySeries = new XYChart.Series<>();
+//            XYChart.Series<String, Double> expenseCategorySeries = new XYChart.Series<>();
+//
+//            // Create a XYChart.Data object for every category, then add it to the series.
+//            for (int i = 0; i < incomeValueTotal.length; i++) {
+//                incomeCategorySeries.getData().add(new XYChart.Data<>(incomeCategoryNames.get(i), incomeValueTotal[i]));
+//            }
+//            for (int i = 0; i < expenseValueTotal.length; i++) {
+//                expenseCategorySeries.getData().add(new XYChart.Data<>(expenseCategoryNames.get(i), expenseValueTotal[i]));
+//            }
+//
+//            incomeCategoriesBarChart.getData().add(incomeCategorySeries);
+//            expenseCategoriesBarChart.getData().add(expenseCategorySeries);
 //        }
+//        catch (DatabaseException de) {
+//            System.out.println(de.getErrorCodeMessage());
+//        }
+//    }
+
+    @FXML
+    LineChart<String,Number> incomeLineChart;
+    @FXML
+    LineChart<String,Number> expenseLineChart;
+    public void displayIncomeChart() {
+        // first data
+        XYChart.Series<String, Number> seriesA = new XYChart.Series<>();
+        XYChart.Data<String, Number> jan1 = new XYChart.Data<>("Jan", 300);
+        XYChart.Data<String, Number> feb1 = new XYChart.Data<>("Feb", 500);
+        XYChart.Data<String, Number> mar1 = new XYChart.Data<>("Mar", 600);
+        seriesA.getData().addAll(jan1, feb1, mar1);
+        seriesA.setName("Income item a");
+        // next data
+        XYChart.Series<String, Number> seriesB = new XYChart.Series<>();
+        XYChart.Data<String, Number> jan2 = new XYChart.Data<>("Jan", 400);
+        XYChart.Data<String, Number> feb2 = new XYChart.Data<>("Feb", 200);
+        XYChart.Data<String, Number> mar2 = new XYChart.Data<>("Mar", 950);
+        seriesB.getData().addAll(jan2, feb2, mar2);
+        seriesB.setName("Income item b");
+        incomeLineChart.getData().addAll(seriesA, seriesB);
+    }
+
+    public void displayExpenseChart() {
+        // first data
+        XYChart.Series<String, Number> seriesA = new XYChart.Series<>();
+        XYChart.Data<String, Number> jan1 = new XYChart.Data<>("Jan", 300);
+        XYChart.Data<String, Number> feb1 = new XYChart.Data<>("Feb", 500);
+        XYChart.Data<String, Number> mar1 = new XYChart.Data<>("Mar", 600);
+        seriesA.getData().addAll(jan1, feb1, mar1);
+        seriesA.setName("Expense item a");
+        // next data
+        XYChart.Series<String, Number> seriesB = new XYChart.Series<>();
+        XYChart.Data<String, Number> jan2 = new XYChart.Data<>("Jan", 400);
+        XYChart.Data<String, Number> feb2 = new XYChart.Data<>("Feb", 200);
+        XYChart.Data<String, Number> mar2 = new XYChart.Data<>("Mar", 950);
+        seriesB.getData().addAll(jan2, feb2, mar2);
+        seriesB.setName("Expense item b");
+        expenseLineChart.getData().addAll(seriesA, seriesB);
     }
 
     @FXML
     LineChart<String,Number> balanceLineChart;
-    public void displayLineChart() {
+    public void displayBalanceChart() {
         // first data
         XYChart.Series<String, Number> seriesA = new XYChart.Series<>();
         XYChart.Data<String,Number> jan1 = new XYChart.Data<>("Jan",300);
